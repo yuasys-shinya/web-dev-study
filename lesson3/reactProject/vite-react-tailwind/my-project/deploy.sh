6b68d3f65308d6fb7925c3f5467ff708002d6e19
yuasys-shinya <52190687+yuasys-shinya@users.noreply.github.com>
2022-11-30 17:03:53 +0900

    vite deploy

--- lesson3/reactProject/vite-react-tailwind/my-project/deploy.sh
+++ lesson3/reactProject/vite-react-tailwind/my-project/deploy.sh
@@ -1,37 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
- 
-
-
-
- 
-
-
-
-
 #!/usr/bin/env sh
 
 # エラー時は停止
@@ -59,5 +25,6 @@ git commit -m 'deploy'
 
 # https://<USERNAME>.github.io/<REPO> にデプロイする場合
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:yuasys-shinya/web-dev-study/lesson3/reactProject/vite-react-tailwind/my-project/.git main:gh-pages
 
 cd -
