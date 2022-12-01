3009460f3348d539551b4782d807dc56a4f63b91
yuasys-shinya <52190687+yuasys-shinya@users.noreply.github.com>
2022-12-01 14:23:51 +0900

    pc.jpg update

--- lesson3/reactProject/vite-react-tailwind/my-project/src/App.jsx
+++ lesson3/reactProject/vite-react-tailwind/my-project/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <header className='text-gray-700 border-b border-gray-200'>
       <div className='container flex mx-auto p-5 flex-col md:flex-row items-center'>
         <a href="#" className='font-medium text-gray-900 mb-4 md:mb-0'>
-          <span className='text-xl ml-3'>WebDevGorilla</span>
+          <span className='text-xl ml-3'>webDevGorilla</span>
         </a>
         <nav className='md:ml-auto text-base'>
           <a href="#home" className='mr-5 hover:text-blue-400 duration-300'>Home</a>
